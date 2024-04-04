241be4783817d44361161f7e638c92f78443637f
padmapriyar1504 <139487530+padmapriyar1504@users.noreply.github.com>
2024-04-04 18:03:49 +0530

    Time: 57 ms (45.35%) | Memory: 16.6 MB (90.78%) - LeetSync

---
+++ 2-add-two-numbers/add-two-numbers.py
@@ -0,0 +1,27 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        dummy = ListNode()
+        temp = dummy
+        carry = 0
+        
+        while l1 or l2 or carry:
+            sum_val = carry
+            if l1:
+                sum_val += l1.val
+                l1 = l1.next
+            if l2:
+                sum_val += l2.val
+                l2 = l2.next
+            
+            carry = sum_val // 10
+            digit = sum_val % 10
+            
+            temp.next = ListNode(digit)
+            temp = temp.next
+        
+        return dummy.next
