5a16ade54714e95e39c20cca71441ef41dbeb3b8
padmapriyar1504 <139487530+padmapriyar1504@users.noreply.github.com>
2024-05-26 23:45:22 +0530

    Time: 34 ms (64.81%) | Memory: 16.4 MB (99.68%) - LeetSync

--- 433-minimum-genetic-mutation/minimum-genetic-mutation.py
+++ 433-minimum-genetic-mutation/minimum-genetic-mutation.py
@@ -1,19 +1,23 @@
+from collections import deque
+
 class Solution:
-    def minMutation(self, start: str, end: str, bank: List[str]) -> int:
-        dic=defaultdict(lambda :0)
-        lst=[[start,0]]
-        dic[start]=1
-        while lst:
-            x,d=lst.pop(0)
-            if x==end:
-                return d
-            for i in range(len(bank)):
-                ct=0
-                for j in range(8):
-                    if x[j]!=bank[i][j]:
-                        ct+=1
-                if ct==1:
-                    if dic[bank[i]]==0:
-                        lst.append([bank[i],d+1])
-                        dic[bank[i]]=1
-        return -1
+  def minMutation(self, startGene, endGene, bank):
+    
+    visited = set()
+    queue = deque([(startGene, 0)])  
+
+    while queue:
+      gene, level = queue.popleft()
+      visited.add(gene)
+
+      if gene == endGene:
+        return level
+
+      for i in range(len(gene)):
+        for char in ['A', 'C', 'G', 'T']:
+          new_gene = gene[:i] + char + gene[i+1:]
+          if new_gene in bank and new_gene not in visited:
+            queue.append((new_gene, level + 1))
+
+    return -1 
+
